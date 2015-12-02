f41753d93bc978eec9a5c8a5446138748739c49d
em <michael@joinbox.com>
2015-12-02 20:10:30 +0100

    added more features, buugfixes

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 	module.exports = require('./lib/Playr');
     module.exports.Scenario = require('./lib/Scenario');
     module.exports.ScenarioContext = require('./lib/ScenarioContext');
+    module.exports.ResponseMiddleware = require('./lib/ResponseMiddleware');
 
 })();
